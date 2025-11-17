1cb9bbda8a3c7b390aac5a799589932223e74056
Jonas Perez <00jonasperez@gmail.com>
2025-11-17 23:30:29 +0800

    chore: install daisyui and delete template stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
   subsets: ["latin"],
 });
 
@@ -24,10 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${notoSans.variable} antialiased`}>
+        <div className="flex min-h-screen items-center justify-center">
+          {children}
+        </div>
       </body>
     </html>
   );
